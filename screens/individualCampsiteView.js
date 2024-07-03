bed4e2c888ebf57ff35e6f31060fafa778eb611a
Valentyna Borysenko <valentynaborysenko11@gmail.com>
2024-07-03 18:59:29 +0300

    Created and styled Individual campsite view

--- screens/individualCampsiteView.js
+++ screens/individualCampsiteView.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
+import { Button, View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import Header from "../components/Header";
-import SearchCampsiteView from "./searchCampsiteView";
+import { FlatList } from "react-native-web";
+import Images from "../components/images";
+
 
 const IndividualCampsiteView = ({campsite_id}) => {
+    const username = "Guest"; 
+  
+    const handleUsernamePress = () => {
+      
+    };
     return (
         <View style={styles.container}>
-            <Header />
-            
+            <Header title="Campsite Name"/>
+            <TouchableOpacity style={styles.usernameButton} onPress={handleUsernamePress}>
+        <Text style={styles.buttonText}>{username}</Text>
+      </TouchableOpacity>
+      
+    <Images />
+    
+      
+      <View style={styles.sectionContainer}>
+
+        <Text style={styles.sections}>Address</Text>
+        <Text>This is where address goes</Text>
+        <Text style={styles.sections}>
+            Facilities</Text>
+<Text> These are the facilities the campsite has </Text>
+<Text style={styles.sections}>
+            Prices </Text>
+        <Text>These are the prices</Text>
+
+        </View>
         </View>
     );
 }
@@ -19,6 +44,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 16,
     },
+    usernameButton: {
+        position: 'absolute',
+        top: 70,
+        right: 20,
+        backgroundColor: '#2ECC71',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        zIndex: 10, 
+      },
+     sections: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      sectionContainer: { width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 50, 
+      backgroundColor: 'darkseagreen',
+    },
+    images: {
+        width: 150,
+        height: 150,
+        marginBottom: 10,
+        borderRadius: 8, 
+        
+    }
 });
 
 export default IndividualCampsiteView;
