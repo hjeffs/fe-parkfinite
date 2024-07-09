9088059f2591d0083b9209e4935355d3e33374fd
Richard Harrison <r.harr.k@gmail.com>
2024-07-09 12:51:59 +0100

    result of new .gitignore

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,26 +1,44 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {View, Text} from "react-native";
+import { View, Text } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import SearchCampsiteView from "./screens/searchCampsiteView";
 import IndividualCampsiteView from "./screens/individualCampsiteView";
-
+import PostCampsiteView from "./screens/postCampsiteView";
 
 const Stack = createNativeStackNavigator();
 
-const StackNavigator = () =>{
-    return(
-        <Stack.Navigator screenOptions = {{headerShown: false}}>
-            <Stack.Group>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="SearchCampsiteView" component={SearchCampsiteView} options={{ title: 'Search Campsite' }} />
-            </Stack.Group>
-            <Stack.Group> 
-            <Stack.Screen name="IndividualCampsiteView" component={IndividualCampsiteView} options={{ title: 'View Campsite' }}  />
-            </Stack.Group>
-        </Stack.Navigator>
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Group>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SearchCampsiteView"
+          component={SearchCampsiteView}
+          options={{ title: "Search Campsite" }}
+        />
+      </Stack.Group>
+      <Stack.Group>
+        <Stack.Screen
+          name="IndividualCampsiteView"
+          component={IndividualCampsiteView}
+          options={{ title: "View Campsite" }}
+        />
+      </Stack.Group>
+      <Stack.Group>
+        <Stack.Screen
+            name="PostCampsiteView"
+            component={PostCampsiteView}
+            options={{ title: "Post Campsite"}}
+        />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
 
-    )
-}
-
-export default StackNavigator
+export default StackNavigator;
--- screens/searchCampsiteView.js
+++ screens/searchCampsiteView.js
@@ -1,62 +1,58 @@
 import React from "react";
 import { Button, View, Text, SafeAreaView } from "react-native";
-import Map from './mapView'
+import Map from "../components/Map";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native";
-import SearchBarInput from '../components/elements'
+import SearchBarInput from "../components/elements";
 import IndividualCampsiteView from "./individualCampsiteView";
 import { UserContext } from "../utils/UserContext";
-import { useContext } from 'react';
-
-
+import { useContext } from "react";
+import Header from "../components/Header";
 
 const SearchCampsiteView = ({ navigation }) => {
+  const { user, setUser } = useContext(UserContext);
 
-  const {user, setUser} = useContext(UserContext)
-    const handleUsernamePress = () => {
-      
-    };
-    return (
-        <View style={styles.container}>
-       <Map /> 
-      <TouchableOpacity style={styles.usernameButton} onPress={handleUsernamePress}>
-        <Text style={styles.buttonText}>{user}</Text>
-      </TouchableOpacity>
-      <SearchBarInput />
-    </View>
+  const handleUsernamePress = () => {};
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Map />
+        <Header subtitle={"Post a new campsite "}></Header>
+        <SearchBarInput />
+      </View>
+    </>
   );
 };
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    usernameButton: {
-      position: 'absolute',
-      top: 20,
-      right: 20,
-      backgroundColor: '#2ECC71',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-      zIndex: 10, 
-    },
-    continueButton: {
-      position: 'absolute',
-      top: 20,
-      left: 20,
-      backgroundColor: '#3498DB',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-      borderRadius: 5,
-      zIndex: 10, 
-    },
-    buttonText: {
-      color: '#FFFFFF',
-      fontSize: 16,
-    },
-    
-  });
-  
-  
-export default SearchCampsiteView
+  container: {
+    justifyContent: "flex",
+    flex: 1,
+  },
+  usernameButton: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    backgroundColor: "#2ECC71",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    zIndex: 10,
+  },
+  continueButton: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    backgroundColor: "#3498DB",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    zIndex: 10,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+});
 
+export default SearchCampsiteView;
